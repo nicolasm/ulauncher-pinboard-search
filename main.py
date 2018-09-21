13c7812ba78f1ebab0f90a25c4b3c268f6d0e3e4
Nicolas Meier <ws@nclsmr.com>
2018-09-21 21:38:55 +0200

    Add async download of Pinboard bookmarks

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from ulauncher.api.shared.event import KeywordQueryEvent, PreferencesEvent, Pref
 from ulauncher.api.shared.item.ExtensionResultItem import ExtensionResultItem
 
 from search import search_json_bookmarks
-
+from pinboard import start_async_pinboard_download
 
 class PinboardSearchExtension(Extension):
 
@@ -18,21 +18,19 @@ class PinboardSearchExtension(Extension):
         self.subscribe(PreferencesEvent, PreferencesEventListener())
         self.subscribe(PreferencesUpdateEvent, PreferencesUpdateEventListener())
 
-json_bookmark_file = "/home/nicolas/Documents/Pinboard/Pinboard.json"
-
 class KeywordQueryEventListener(EventListener):
 
     def on_event(self, event, extension):
         search_value = event.get_argument()
 
-        bookmarks = search_json_bookmarks(search_value, json_bookmark_file)
+        bookmarks = search_json_bookmarks(search_value, extension.json_bookmark_file)
 
         hosts = []
         items = []
         if bookmarks:
             for bookmark in bookmarks[:extension.limit]:
                 if extension.aggregate:
-                    hostname = getHostname(bookmark.url)
+                    hostname = get_hostname(bookmark.url)
                     if (not hosts.__contains__(hostname)):
                         hosts.append(hostname)
                 else:
@@ -44,7 +42,7 @@ class KeywordQueryEventListener(EventListener):
         if extension.aggregate:
             for host in hosts[:extension.limit]:
                 items.append(ExtensionResultItem(icon='images/icon.png',
-                                                 name=getName(host),
+                                                 name=get_name(host),
                                                  description=host.encode("utf8"),
                                                  on_enter=HideWindowAction()))
 
@@ -55,11 +53,20 @@ class PreferencesEventListener(EventListener):
         try:
             n = int(event.preferences['limit'])
             aggregate = event.preferences['aggregate']
+            json_bookmark_file = event.preferences['pinboard_bookmark_file']
+            pinboard_api_token = event.preferences['pinboard_api_token']
         except:
             n = 10
             aggregate = False
+            json_bookmark_file = '/tmp/Pinboard.json'
+            pinboard_api_token = 'username:token'
+
         extension.limit = n
         extension.aggregate = (aggregate == "true")
+        extension.json_bookmark_file = json_bookmark_file
+        extension.pinboard_api_token = pinboard_api_token
+
+        start_async_pinboard_download(extension.pinboard_api_token, extension.json_bookmark_file)
 
 class PreferencesUpdateEventListener(EventListener):
     def on_event(self,event,extension):
@@ -71,22 +78,24 @@ class PreferencesUpdateEventListener(EventListener):
                 pass
         elif event.id == 'aggregate':
             extension.aggregate = (event.new_value == "true")
+        elif event.id == 'pinboard_bookmark_file':
+            extension.json_bookmark_file = event.new_value
+        elif event.id == 'pinboard_api_token':
+            extension.pinboard_api_token = event.new_value
 
-def getHostname(str):
+def get_hostname(str):
     url = str.split('/')
     if len(url) > 2:
         return url[2]
     else:
         return 'Unknown'
 
-def getName(hostname):
+def get_name(hostname):
     dm = hostname.split('.')
-    #   Remove WWW
     if dm[0]=='www':
         i = 1
     else:
         i = 0
-    #   Join remaining domains and capitalize
     return ''.join(dm[i:len(dm)-1]).title()
 
 
---
+++ pinboard.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+import requests
+import simplejson as json
+import os.path, time
+import tempfile
+from shutil import copyfile
+from threading import Thread
+
+PINBOARD_API_ALL_POSTS_URL = 'https://api.pinboard.in/v1/posts/all?format=json&auth_token=%s'
+
+
+def start_async_pinboard_download(token, path_to_file):
+    thread = Thread(target = download_pinboard_bookmarks_to_file, args= (token, path_to_file))
+    thread.start()
+
+def download_pinboard_bookmarks_to_file(token, path_to_file):
+    if not os.path.isfile(path_to_file) or is_older_than_one_day(path_to_file):
+        r = requests.get(PINBOARD_API_ALL_POSTS_URL % token)
+
+        temp_file = tempfile.NamedTemporaryFile()
+        with open(temp_file.name, 'w') as outfile:
+            json.dump(r.json(), outfile, sort_keys=True, indent=4, separators=(',', ': '))
+
+        copyfile(temp_file.name, path_to_file)
+        temp_file.close()
+
+
+def is_older_than_one_day(path_to_file):
+    return (time.time() - os.path.getmtime(path_to_file) > 60)
